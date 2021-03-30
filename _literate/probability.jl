8ce57187a9ed71ec7bd27122e706d168195642e4
arzwa <arzwa@psb.vib-ugent.be>
2021-03-30 13:46:05 +0200

    update dev

---
+++ _literate/probability.jl
@@ -0,0 +1,18 @@
+# # Brief probability recap
+#
+# We will need some basic notions of probability theory in order to be able to
+# discuss models and methods in statistical phylogenetics. Here I provide a
+# *very* brief and *informal* recap of some important aspects of probability
+# theory. Needless to say, this neither does justice to the philosophical
+# subtlety of notions such as 'probability', 'plausibility', 'chance' or
+# randomness, nor does it to the mathematically sophisticated formal theory
+# of probability.
+#
+# There are multiple ways of thinking about probabilities, and this is a source
+# of endless controversy, debate and confusion. Intuitively, we may consider
+# two not unrelated notions:
+#
+# 1. The probability of an *event* is the *relative frequency* of that event
+#    in a large number of trials.
+# 2. The probability of a proposition is the degree of belief some rational 
+#    agent has that the proposition is true
